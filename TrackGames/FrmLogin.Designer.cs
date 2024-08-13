bbd72e9dca5f2d07c8ef97220be19b55ef263b7e
lucassantos251 <lucas.bats@gmail.com>
2024-08-13 08:49:16 -0300

    Adicionar arquivos de projeto.

---
+++ TrackGames/FrmLogin.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TrackGames
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mlblLogin = new MaterialSkin.Controls.MaterialLabel();
+            this.mlblSenha = new MaterialSkin.Controls.MaterialLabel();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.mbtnLogin = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // mlblLogin
+            // 
+            this.mlblLogin.AutoSize = true;
+            this.mlblLogin.Depth = 0;
+            this.mlblLogin.Font = new System.Drawing.Font("Roboto", 11F);
+            this.mlblLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.mlblLogin.Location = new System.Drawing.Point(86, 118);
+            this.mlblLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mlblLogin.Name = "mlblLogin";
+            this.mlblLogin.Size = new System.Drawing.Size(50, 19);
+            this.mlblLogin.TabIndex = 0;
+            this.mlblLogin.Text = "Login:";
+            // 
+            // mlblSenha
+            // 
+            this.mlblSenha.AutoSize = true;
+            this.mlblSenha.Depth = 0;
+            this.mlblSenha.Font = new System.Drawing.Font("Roboto", 11F);
+            this.mlblSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.mlblSenha.Location = new System.Drawing.Point(86, 149);
+            this.mlblSenha.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mlblSenha.Name = "mlblSenha";
+            this.mlblSenha.Size = new System.Drawing.Size(54, 19);
+            this.mlblSenha.TabIndex = 1;
+            this.mlblSenha.Text = "Senha:";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(151, 116);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(158, 21);
+            this.tbLogin.TabIndex = 2;
+            // 
+            // tbSenha
+            // 
+            this.tbSenha.Location = new System.Drawing.Point(151, 147);
+            this.tbSenha.Name = "tbSenha";
+            this.tbSenha.Size = new System.Drawing.Size(158, 21);
+            this.tbSenha.TabIndex = 3;
+            this.tbSenha.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbSenha_KeyUp);
+            // 
+            // mbtnLogin
+            // 
+            this.mbtnLogin.AutoSize = true;
+            this.mbtnLogin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mbtnLogin.Depth = 0;
+            this.mbtnLogin.Icon = null;
+            this.mbtnLogin.Location = new System.Drawing.Point(172, 177);
+            this.mbtnLogin.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.mbtnLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mbtnLogin.Name = "mbtnLogin";
+            this.mbtnLogin.Primary = false;
+            this.mbtnLogin.Size = new System.Drawing.Size(61, 36);
+            this.mbtnLogin.TabIndex = 4;
+            this.mbtnLogin.Text = "Login";
+            this.mbtnLogin.UseVisualStyleBackColor = true;
+            this.mbtnLogin.Click += new System.EventHandler(this.mbtnLogin_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.mbtnLogin);
+            this.Controls.Add(this.tbSenha);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.mlblSenha);
+            this.Controls.Add(this.mlblLogin);
+            this.Font = new System.Drawing.Font("Unispace", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrackGame - Login";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel mlblLogin;
+        private MaterialSkin.Controls.MaterialLabel mlblSenha;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbSenha;
+        private MaterialSkin.Controls.MaterialFlatButton mbtnLogin;
+    }
+}
---
+++ TrackGames/FrmLogin.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace TrackGames
+{
+    public partial class FrmLogin : MaterialForm
+    {
+        #region Variaveis
+        private SqlConnection conexaoSQLServer;
+        SqlCommand sqlCmd = default(SqlCommand);
+        private string conexaoSQL = @"Data Source=CELESTIA\SQLEXPRESS;Initial Catalog = GameTracker; Integrated Security = True;";
+        private int idUsuario = 0;
+        #endregion
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+
+            // Create a material theme manager and add the form to manage (this)
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+
+            conexaoSQLServer = new SqlConnection(conexaoSQL);
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login()
+        {
+            if (tbLogin.Text.Length <= 0 || tbSenha.Text.Length <= 0)
+            {
+                MessageBox.Show("Login e Senha não podem ser vazios.");
+            }
+            else
+            {
+                try
+                {
+                    SqlCommand CmdSelect = new SqlCommand("SELECT id_usuario, senha_usuario FROM Usuario WHERE nome_usuario=@usuario", this.ConexaoSQLServer);
+                    CmdSelect.Parameters.Add("@usuario", SqlDbType.VarChar);
+                    CmdSelect.Parameters["@usuario"].Value = tbLogin.Text;
+
+                    this.conexaoSQLServer.Open();
+
+                    using (SqlDataReader CmdReader = CmdSelect.ExecuteReader())
+                    {
+                        while (CmdReader.Read())
+                        {
+                            //leitura e comparação de usuário e senha
+                            if (tbSenha.Text == CmdReader["senha_usuario"].ToString())
+                            {
+                                if (CmdReader["id_usuario"].GetType().ToString() != "System.DBNull")
+                                {
+                                    idUsuario = (int)CmdReader["id_usuario"];
+
+                                    //login bem sucedido, abrir nova janela
+                                    this.Hide();
+                                    //FrmGameTracker frmGameTracker = new FrmGameTracker(idUsuario);
+                                    //frmGameTracker.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Erro ao carregar dados do usuário");
+                                    return;
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Senha incorreta.");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    this.conexaoSQLServer.Close();
+                }
+            }
+        }
+
+        private void mbtnLogin_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void tbSenha_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Login();
+            }
+        }
+    }
+}
